f7450592e8245acc1031743fc9a0eb036bbfe7de
Kuo <hmouraoussama@gmail.com>
2022-10-29 21:21:43 +0100

    init the project

---
+++ src/main/java/dev/kuo/lostarktracker/LostarkTrackerApplication.java
@@ -0,0 +1,13 @@
+package dev.kuo.lostarktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostarkTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostarkTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kuo/lostarktracker/LostarkTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kuo.lostarktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostarkTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
